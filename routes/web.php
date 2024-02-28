84de3c9469d7a4a7e87b69c0381efbaedf1132cf
Jesica Oktavia <jesicaoktavia737@gmail.com>
2024-02-28 20:53:10 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\PenjualanController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/home', [HomeController::class, 'home']);
+
+Route::prefix('category')->group(function () {
+    Route::get('/food-beverage', [ProductController::class, 'foodBeverage'])->name('food-beverage');
+    Route::get('/beauty-health', [ProductController::class, 'beautyHealth'])->name('beauty-health');
+    Route::get('/home-care', [ProductController::class, 'homeCare'])->name('home-care');
+    Route::get('/baby-kid', [ProductController::class, 'babyKid'])->name('baby-kid');
+});
+
+Route::get('/user/{id}/name/{name}', [UserController::class, 'show']);
+
+Route::get('/penjualan', [PenjualanController::class, 'show']);
